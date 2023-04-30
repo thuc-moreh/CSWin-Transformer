5d1fc231c3606e3a11cb2df696739f32371b7c80
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-30 17:48:37 +0900

    add training logs

--- run_main.sh
+++ run_main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash train.sh 8 --data /nas/common_data/imagenet_100cls --model CSWin_64_12211_tiny_224 -b 350 --lr 2e-3 --weight-decay .05 --img-size 224 --model-ema-decay 0.99984 --epochs 1 --drop-path 0.2 > cswin_transformer_medium128gb_bs350.text
+bash train.sh 8 --data /nas/common_data/imagenet_100cls --model CSWin_64_12211_tiny_224 -b 700 --lr 2e-3 --weight-decay .05 --img-size 224 --model-ema-decay 0.99984 --epochs 1 --drop-path 0.2 
+tmux capture-pane -pS -300 > cswin_transformer_bs700_withoutamp.log
